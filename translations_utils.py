0b853cc182a3a64add76ac2e04cdb90d46ecec6a
Nsfr750 <nsfr750@gmail.com>
2025-05-22 23:29:23 +0200

    Create translations_utils.py

---
+++ translations_utils.py
@@ -0,0 +1,19 @@
+"""
+translations_utils.py
+Utility class for managing translations and language switching in a modular way.
+"""
+
+class TranslationsManager:
+    def __init__(self, translations_dict, default_lang='EN'):
+        self.translations = translations_dict
+        self.default_lang = default_lang
+
+    def t(self, key, lang=None):
+        lang = (lang or self.default_lang).upper()
+        return self.translations.get(lang, {}).get(key, self.translations.get(self.default_lang, {}).get(key, key))
+
+    def available_languages(self):
+        return list(self.translations.keys())
+
+    def set_default_lang(self, lang):
+        self.default_lang = lang.upper()

f172946ee9c5ba5e8c763ab1d3aa70b13ac8ee36
Nsfr750 <nsfr750@gmail.com>
2025-05-25 23:26:17 +0200

    -

--- weather.bat
+++
@@ -1,8 +0,0 @@
-python -m nuitka ^
---onefile ^
---plugin-enable=tk-inter ^
---windows-console-mode=disable ^
---windows-icon-from-ico=icon.ico ^
-weather_app.py
-pause
-ren weather_app.exe Weather.exe
